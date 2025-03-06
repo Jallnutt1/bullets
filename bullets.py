366fcacdaa282a81ba89e608f2c7e0e2db7b7396
Jason Allnutt <30048057+Jallnutt1@users.noreply.github.com>
2025-03-05 20:19:41 -0500

    first commit

--- bullets.py
+++ bullets.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import csv
 
+# PS command to create a file: New-Item -ItemType file database.csv
 tsf = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 bullet = input("What did you accomplish? ")
 print(f"You entered: {bullet} at {tsf}")
