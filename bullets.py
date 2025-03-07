f5958d3e6df601f23d2884da3300314c7391bd5d
Jason Allnutt <30048057+Jallnutt1@users.noreply.github.com>
2025-03-06 20:06:14 -0500

    Updated Bullets to Read

--- bullets.py
+++ bullets.py
@@ -6,6 +6,12 @@ tsf = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 bullet = input("What did you accomplish? ")
 print(f"You entered: {bullet} at {tsf}")
 row = [tsf,bullet]
-file = open("database.csv", "w")
-writer = csv.writer(file)
-writer.writerow(row)
+with open("database.csv", "a", newline='\n') as file:
+    writer = csv.writer(file)
+    writer.writerow(row)
+    file.close()
+
+with open("database.csv", 'r') as fileRead:
+    csvFile = csv.reader(fileRead)
+    for lines in csvFile:
+        print(lines)
