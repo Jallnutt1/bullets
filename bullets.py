6e3d54a7ee7f591610e4cb9a546023ba09743a70
Jason Allnutt <30048057+Jallnutt1@users.noreply.github.com>
2025-03-08 05:26:35 -0500

    Started my menu

--- bullets.py
+++ bullets.py
@@ -1,6 +1,14 @@
 from datetime import datetime
+from pyfiglet import Figlet
 import csv
 import os.path
+
+f = Figlet(font='larry3d')
+print(f.renderText('Weekly'))
+print(f.renderText('Bullets'))
+
+# https://pypi.org/project/simple-term-menu/
+
 
 if os.path.isfile("C:\\Users\\Jason\\Documents\\bullets\\database.csv"):
     print("database.csv exists")
@@ -8,6 +16,16 @@ else:
     print('database.csv DOES NOT exist. Need to create, one second')
     file = open("database.csv", "x")
     file.close()
+
+print("""
+Select and option:
+1. Add a weekly accomplishment
+2. Print the database
+3. Something else
+""")
+
+selection = input()
+print(f"You selected {selection}")
 
 # PS command to create a file: New-Item -ItemType file database.csv
 tsf = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
