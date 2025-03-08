f3bd05b4fd869267169b780d20ac370473800546
Jason Allnutt <30048057+Jallnutt1@users.noreply.github.com>
2025-03-07 20:50:39 -0500

    checked for database

--- bullets.py
+++ bullets.py
@@ -1,5 +1,13 @@
 from datetime import datetime
 import csv
+import os.path
+
+if os.path.isfile("C:\\Users\\Jason\\Documents\\bullets\\database.csv"):
+    print("database.csv exists")
+else:
+    print('database.csv DOES NOT exist. Need to create, one second')
+    file = open("database.csv", "x")
+    file.close()
 
 # PS command to create a file: New-Item -ItemType file database.csv
 tsf = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
